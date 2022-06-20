10dab22a3fd6ea6db1cef6b41c05e487554c2a87
XuanKhanh <xuankhanh379@gmail.com>
2022-06-20 22:08:18 +0700

    upload

--- src/demoAddObjectToArray.js
+++ src/demoAddObjectToArray.js
@@ -15,20 +15,21 @@
 // const addObject = [...state.users, oject];
 // console.log(addObject);
 
-const rows = [
-    { id: 11, active: 'no' },
-    { id: 22, active: 'yes' },
-    { id: 33, active: 'no' },
-    { id: 44, active: 'no' }
-]
+const rows =
+    [
+        { username: 'pxk', password: '123' },
+        { username: 'pxk2', password: '123' },
+        { username: 'pxk3', password: '123' },
+        { username: 'pxk4', password: '123' }
+    ]
 
-const new_item = { id: 22, active: 'yeah' };
-rows.map((row) => {
-    if (row.id === new_item.id) {
-        row = new_item;
+const new_item = { username: 'pxk', password: '123456' };
+rows.map((p) => {
+    if (p.id === new_item.id) {
+        p = new_item;
     }
-    console.log(row);
+    console.log(p);
 
-    return row;
+    return p;
 });
 
